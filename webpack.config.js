82d19ea1c2ecd4aac486a42a355719d3dae9ce84
ODEit <jose.sabal@yahoo.com>
2018-04-17 12:58:54 -0400

    Ready and moving with the frontend

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './CalendarFrontEnd/client/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: './CalendarFrontEnd/public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
